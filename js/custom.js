1861ac2569955b539e644fcd1038a210f5bdf47d
chilam112094 <87189157+chilam112094@users.noreply.github.com>
2021-09-22 12:36:51 +0700

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+$('#ppp').owlCarousel({
+    loop:true,
+    margin:100,
+//    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+$('#text').owlCarousel({
+    loop:true,
+    margin:150,
+//    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
